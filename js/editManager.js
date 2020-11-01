1ab7050b0e77495da8f32fe3e9d1d171ead534e8
SelinaWernike <63050357+SelinaWernike@users.noreply.github.com>
2020-11-01 19:04:42 +0100

    Flexible layout. Items can be deleted and are scaled. Methods for position manipulation are integrated but not implemented yet.

--- js/editManager.js
+++ js/editManager.js
@@ -1,11 +1,10 @@
 export default class EditManager {
 
     constructor(trackname, sectionname, type) {
-        this.sectionname = sectionname;
-        this.trackname = trackname;
         this.type = type;
         this.trackNode = document.querySelector('#' + trackname);
         this.sectionNode = document.querySelector('#' + sectionname);
+        this.elements = [];
     }
 
     initializeTrack() {
@@ -14,18 +13,52 @@ export default class EditManager {
         })
         this.sectionNode.addEventListener('drop', (e) => {
             e.preventDefault();
-            let container = document.createElement('div');
-            container.classList.add('column');
             let childData = e.dataTransfer.getData('html');
-            container.innerHTML = childData;
-            let nameElement = container.querySelector(".fileNameText");
-            let name = nameElement.innerHTML;
-            console.log(name);
-            console.log(childData);
-            if(name.includes(this.type)) {
-                this.trackNode.appendChild(container);
+            if(childData) {
 
+                let container = document.createElement('div');
+                container.classList.add('column');
+                container.innerHTML = childData;
+                let nameElement = container.querySelector(".fileNameText");
+                let name = nameElement.innerHTML;
+                console.log(name);
+                console.log(childData);
+                if(name.includes(this.type)) {
+                    this.elements = this.sectionNode.querySelectorAll(".column");
+                    var width = 100 / (this.elements.length + 1);
+                    if(this.elements.length > 0 && width > 1) {
+                        container.style.width = width + "%";
+                        this.elements.forEach(col => {
+                            console.log(width);
+                            col.style.width = width + "%";
+                        });
+                    }
+                    this.trackNode.appendChild(container);
+                    this.elements = this.sectionNode.querySelectorAll(".column");
+                    this.removeElement();
+                    this.addDragNDrop()
+                    
+                }
             }
+        });
+    }
+
+    removeElement() {
+        this.elements.forEach(item => {
+            let close = item.querySelector("span");
+            close.onclick = function() {
+                item.parentNode.removeChild(item);
+            };
+        });
+    }
+
+    addDragNDrop() {
+        this.elements.forEach(item => {
+            item.setAttribute("draggable", true);
+           /* item.addEventListener("dragstart", (e) => {
+                e.dataTransfer.setData("trackItem", e.target.outerHTML);
+            }); */
+
         });
     }
 
