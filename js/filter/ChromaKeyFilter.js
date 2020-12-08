37547dd8375c97e7b939a39743d9a172c23168ba
BlackAnn <anna.schwarz4@yahoo.de>
2020-12-08 12:01:12 +0100

    initial commit, small change to ChromaKeyFilter.js

--- js/filter/ChromaKeyFilter.js
+++ js/filter/ChromaKeyFilter.js
@@ -2,13 +2,25 @@ export default class ChromaKeyFilter {
 
     apply(frame) {
         let l = frame.data.length / 4;
+        let x = 1;
+
+        /*switch (color) {
+            case 'r':
+                x = 0;
+                break;
+            case 'g':
+                x = 1;
+                break;
+            case 'b':
+                x = 1;
+                break;
+        }*/
+
         for (let i = 0; i < l; i++) {
-            let r = frame.data[i * 4 + 0];
-            let g = frame.data[i * 4 + 1];
-            let b = frame.data[i * 4 + 2];
+            let c = frame.data[i * 4 + x];
 
             // change all greens to white
-            if (g > 100) frame.data[i * 4 + 3] = 0;
+            if (c > 100) frame.data[i * 4 + x] = 0;
         }
     }
 }
